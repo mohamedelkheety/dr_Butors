63af80cfaaf5b6b2a778c566f228725913046e8c
mohamedelkheety <mohamedelkheaty97@gmail.com>
2024-01-31 22:56:05 +0200

    edit splach view

--- lib/view/splash_view/splach.dart
+++ lib/view/splash_view/splach.dart
@@ -2,7 +2,6 @@ import 'package:dr_boutros/helper/constants.dart';
 import 'package:flutter/material.dart';
 
 import '../home/home_page.dart';
-import 'widget/animated_text.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -12,13 +11,10 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> with TickerProviderStateMixin {
-  late AnimationController animationController;
-  late Animation<Offset> slideingAnimation;
   @override
   void initState() {
     super.initState();
-    animationMethod();
-    Future.delayed(const Duration(seconds: 4), () {
+    Future.delayed(const Duration(seconds: 3), () {
       Navigator.of(context).pushAndRemoveUntil(
           MaterialPageRoute(builder: (context) {
         return const HomePage();
@@ -27,39 +23,15 @@ class _SplashViewState extends State<SplashView> with TickerProviderStateMixin {
   }
 
   @override
-  void dispose() {
-    animationController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kPrimaryColor,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.24,
-          ),
-          Image.asset(
-            'assets/images/logo.png',
-            height: MediaQuery.of(context).size.height * 0.30,
-          ),
-          AnimatedText(slideingAnimation: slideingAnimation)
-        ],
+      body: Center(
+        child: Image.asset(
+          'assets/images/logonew.png',
+          height: MediaQuery.of(context).size.height * 0.42,
+        ),
       ),
     );
-  }
-
-  void animationMethod() {
-    animationController = AnimationController(
-      duration: const Duration(seconds: 1),
-      vsync: this,
-    );
-    slideingAnimation =
-        Tween<Offset>(begin: const Offset(0, 3), end: Offset.zero)
-            .animate(animationController);
-    animationController.forward();
   }
 }
