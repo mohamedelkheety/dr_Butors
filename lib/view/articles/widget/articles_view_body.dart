d5be894e15d2c4d8e6ad1d7fca0fac8a4b7e0ea1
mohamedelkheety <mohamedelkheaty97@gmail.com>
2025-02-28 21:43:16 +0200

    chore: update build configuration and add additional project files for multiple architectures

--- lib/view/articles/widget/articles_view_body.dart
+++ lib/view/articles/widget/articles_view_body.dart
@@ -41,7 +41,6 @@ class _ArticlesViewBodyState extends State<ArticlesViewBody> {
   @override
   void initState() {
     loadAd();
-
     super.initState();
   }
 
@@ -49,30 +48,46 @@ class _ArticlesViewBodyState extends State<ArticlesViewBody> {
   Widget build(BuildContext context) {
     num size = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Padding(
-          padding: const EdgeInsets.only(right: 8.0, left: 8, bottom: 8),
-          child: Column(
-            spacing: 5,
-            textDirection: TextDirection.rtl,
-            children: [
-              if (isLoaded) BannerContainer(bannerAd: bannerAd),
-              Text(
-                textDirection: TextDirection.rtl,
-                widget.articlesModel.title,
-                style: TextStyle(fontSize: size * 0.05),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Padding(
+                padding: const EdgeInsets.only(right: 8.0, left: 8, bottom: 8),
+                child: Column(
+                  spacing: 5,
+                  textDirection: TextDirection.rtl,
+                  children: [
+                    if (isLoaded) BannerContainer(bannerAd: bannerAd),
+                    Text(
+                      textDirection: TextDirection.rtl,
+                      widget.articlesModel.title,
+                      style: TextStyle(fontSize: size * 0.05),
+                    ),
+                    Image.asset(widget.articlesModel.image),
+                    SelectableText(
+                      textAlign: TextAlign.justify,
+                      widget.articlesModel.content,
+                      style: TextStyle(fontSize: size * 0.044),
+                      textDirection: TextDirection.rtl,
+                    ),
+                  ],
+                ),
               ),
-              Image.asset(widget.articlesModel.image),
-              SelectableText(
-                textAlign: TextAlign.justify,
-                widget.articlesModel.content,
-                style: TextStyle(fontSize: size * 0.044),
-                textDirection: TextDirection.rtl,
-              ),
-            ],
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: InkWell(
+                  onTap: () {
+                    setState(() {
+                      bannerAd?.dispose();
+                      Navigator.of(context).pop();
+                    });
+                  },
+                  child: Icon(Icons.arrow_back_ios)),
+            ),
+          ],
         ),
       ),
     );
